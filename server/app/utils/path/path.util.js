0931e74d3c0c6b58f6dc66d39a075c3bd6ec9776
wjdwnsdud95 <wjdwnsdud95@gmail.com>
2025-12-03 14:49:40 +0900

    251203 path util 내용 추가

--- server/app/utils/path/path.util.js
+++ server/app/utils/path/path.util.js
@@ -7,11 +7,27 @@
 import path from 'path';
 
 function getViewDirPath() {
-  const __dirname = process.env.APP_MODE !== 'dev' ? process.env.APP_DIST_PATH : path.resolve(process.env.APP_DIST_PATH);
-
+  const __dirname =
+    process.env.APP_MODE !== 'dev'
+     ? process.env.APP_DIST_PATH
+       : path.resolve(process.env.APP_DIST_PATH);
   return path.join(__dirname, 'index.html');
+}
+
+function getPostsImagePath() {
+  return process.env.APP_MODE !== 'dev'
+   ? process.env.FILE_POST_IMAGE_PATH
+    : path.resolve(process.env.FILE_POST_IMAGE_PATH);
+}
+
+function getProfilesImagePath() {
+  return process.env.APP_MODE !== 'dev'
+   ? process.env.FILE_USER_PROFILE_PATH
+    : path.resolve(process.env.FILE_USER_PROFILE_PATH);
 }
 
 export default {
   getViewDirPath,
+  getPostsImagePath,
+  getProfilesImagePath,
 }
